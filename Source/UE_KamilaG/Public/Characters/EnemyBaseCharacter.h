16b9dbbd27879f1cc13c39d47206ae9921b20c2b
Kamila Gościńska <kgoscinska@edu.cdv.pl>
2025-11-06 17:22:44 +0100

    Bug fixing and players class

---
+++ Source/UE_KamilaG/Public/Characters/EnemyBaseCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/MyBaseCharacter.h"
+#include "EnemyBaseCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_KAMILAG_API AEnemyBaseCharacter : public AMyBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
